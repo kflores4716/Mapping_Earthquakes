97d1cf8f54a2757f9c8f7de35bb9a9c86cf34190
kflores4716 <kflores4716@gmail.com>
2022-03-15 12:33:09 -0600

    Adding circleMarker code

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -14,11 +14,3 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
-// // We create the tile layer that will be the background of our map.
-// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     accessToken: API_KEY
-// });
-// // Then we add our 'graymap' tile layer to the map.
-// streets.addTo(map);
